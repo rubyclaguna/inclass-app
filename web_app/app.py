0f75aba80c2f078d9107da341adfc42e73049f57
rubyclaguna <52681236+rubyclaguna@users.noreply.github.com>
2020-02-15 13:16:34 -0800

    configure users route

--- web_app/app.py
+++ web_app/app.py
@@ -1,8 +1,22 @@
 
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "Hello World!"
+
+@app.route("/about")
+def about(): 
+    return "About Me"
+
+@app.route("/users")
+@app.route("/users.json")
+def users():
+    users = [
+        {"id":1, "name": "First User"},
+        {"id":2, "name": "Second User"},
+        {"id":3, "name": "Third User"},
+    ]
+    return jsonify(users)
