d961461dfc9513b97fce9051cefa3c058dde2053
rubyclaguna <52681236+rubyclaguna@users.noreply.github.com>
2020-02-15 21:46:54 -0800

    Setup database

--- web_app/app.py
+++ web_app/app.py
@@ -1,7 +1,25 @@
 
 from flask import Flask, jsonify, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
+app.config["CUSTOM_VAR"] = 5
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_200.db"
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
 
 @app.route("/")
 def index():
