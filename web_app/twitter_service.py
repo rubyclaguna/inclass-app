2e3002c5a4047f1b07c14d8e6a348ca6c4bd7a9a
rubyclaguna <52681236+rubyclaguna@users.noreply.github.com>
2020-02-17 20:39:53 -0800

    twitter service

---
+++ web_app/twitter_service.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import tweepy
+
+load_dotenv()
+
+twitter_api_key = os.getenv("twitter_api_key", default="OOPS")
+twitter_api_secret = os.getenv("twitter_api_secret", default="OOPS")
+twitter_access_token = os.getenv("twitter_access_token", default="OOPS")
+twitter_access_token_secret = os.getenv("twitter_access_token_secret", default="OOPS")
+
+auth = tweepy.OAuthHandler(twitter_api_key, twitter_api_secret)
+print(type(auth))
+auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+
+client = tweepy.API(auth)
+print(type(client))
+print(dir(client))
+print("----------")
+public_tweets = client.home_timeline()
+for tweet in public_tweets:
+    print(type(tweet), tweet.text)
